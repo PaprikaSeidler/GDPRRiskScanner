ec0064420f65fc781041a63c9d45e91e720d0a6b
Paprika Seidler <paprika.seidler@hotmail.com>
2026-04-30 09:24:14 +0200

    Implement email finding functionality and add sample email data

--- scanner/src/Finders/FindEmail.ps1
+++ scanner/src/Finders/FindEmail.ps1
@@ -1,7 +1,47 @@
-class FindEmail {
+function  Find-Email {
+    param (
+        [string]$ScanPath = "$PSScriptRoot\..\..\..\LocalTestFolder",
+        [string[]]$FileFilter = @("*.txt")
+    )
+
     # Regular expression pattern for matching email addresses
     $regexEmail = '[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
 
-    # Regular expression options for case-insensitive matching
-    [System.Text.RegularExpressions.RegexOptions]$regexOptions = [System.Text.RegularExpressions.RegexOptions]::IgnoreCase
+    $files = Get-ChildItem -Path $ScanPath -Recurse -File -Include $FileFilter
+
+    $results = [System.Collections.Generic.List[PSCustomObject]]::new()
+
+    foreach ($file in $files) {
+        $matchCount = 0
+        $lineNumber = 0
+        $matchedLines = [System.Collections.Generic.List[Int32]]::new()
+        foreach ($line in Get-Content $file.FullName) {
+            $lineNumber++
+
+            # Apply regex to each line, but do not store the actual email addresses
+            $Matches = [regex]::Matches($line, $regexEmail)
+
+            if ($Matches.Count -gt 0) {                    
+                $matchedLines.Add($lineNumber)             
+            }
+
+            $matchCount += $Matches.Count
+        }
+
+        if ($matchCount -gt 0) {
+            $results.Add([PSCustomObject]@{
+                FileName   = $file.Name
+                FilePath   = $file.FullName
+                MatchCount = $matchCount
+                LineNumbers = $matchedLines
+            })
+        }
+    }
+
+    return $results
 }
+
+# Automatically call the function when the script is run
+$result = Find-Email
+$result | Format-List FileName, FilePath, MatchCount, LineNumbers
+Write-Host "LineNumbers : $($result.LineNumbers -join ', ')"
